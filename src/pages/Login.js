ed9491b8e514c19b80d43df4222922be7641b35e
leoplazavzla <Caracter$$123>
2022-03-07 22:03:33 -0300

    Fix layout of the Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,14 @@
 import React, {useState} from "react"
 import BaseLayout from "../BaseLayout";
 import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
 import Strings from "../resources/Strings";
-import {Button} from "@mui/material";
+import {Button, Typography, Box, Container, TextField, Avatar, Link} from "@mui/material";
 import {auth} from "../firebase/firebaseConfig"
-import {logOut, login} from "../Api/authentication"
 import {useNavigate} from "react-router-dom/";
 import {useAuth} from "../contexts/AuthContext"
+import LoginIcon from '@mui/icons-material/Login';
 import Alert from '@mui/material/Alert';
-import {onAuthStateChanged} from "firebase/auth";
+import {NavLink} from "react-router-dom";
 
 const Login = () => {
     let navigate = useNavigate();
@@ -33,60 +32,71 @@ const Login = () => {
 
     return(
         <BaseLayout>
-            <Grid container direction="column" spacing={2}>
-                <Grid item>
-                    <div><h1>{Strings.login.name}</h1></div>
-                </Grid>
-                <Grid item>
-                    <TextField
-                        label={Strings.login.insertUserName}
-                        value={loginEmail || ''}
-                        onChange={
-                            (e) => {
-                                setLoginEmail(e.target.value)
-                            }
-                        }
-                        style={{width: '60%', paddingBottom: '15px'}}
-                        error={false}>
-
-                    </TextField>
-                </Grid>
-                <Grid item>
-                    <TextField
-                        label={Strings.login.insertPassword}
-                        type={"password"}
-                        value={loginPassword || ''}
-                        onChange={(e) => setLoginPassword(e.target.value)}
-                        style={{width: '60%', paddingBottom: '15px'}}
-                        error={false}>
-
-                    </TextField>
-
-                </Grid>
-                <Grid item>
-                    <Button
-                        disabled={loading}
-                        onClick={singIn}
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LoginIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        {Strings.login.name}
+                    </Typography>
+                    <Box
+                        component="form"
+                        noValidate
+                        sx={{ mt: 1 }}
                     >
-                        {Strings.login.signIn}
-                    </Button>
-                </Grid>
-                    <Grid item>
-                                Don't have an account?
-                            <Button
-                                onClick={() => navigate("/register")}
-                            >
-                                {Strings.register.createAccount}
-                            </Button>
-                    </Grid>
-                </Grid>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label={Strings.login.insertUserName}
+                            value={loginEmail || ''}
+                            onChange={
+                                (e) => {
+                                    setLoginEmail(e.target.value)
+                                }
+                            }
+                            error={false}
+                        />
+
+                        <TextField
+                            fullWidth
+                            label={Strings.login.insertPassword}
+                            type={"password"}
+                            value={loginPassword || ''}
+                            onChange={(e) => setLoginPassword(e.target.value)}
+                            error={false}
+                        />
+                        <Button
+                            type="submit"
+                            disabled={loading}
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={singIn}
+                        >
+                            {Strings.login.signIn}
+                        </Button>
+                        <Grid container justifyContent={"center"}>
+
+                            <Grid item>
+                                <NavLink to="/register" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </NavLink>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
         </BaseLayout>
     )
 }
 
 export default Login;
-
-/*
-onClick={() => {
-    // login(auth, registerEmail, registerPassword)
-}}*/
