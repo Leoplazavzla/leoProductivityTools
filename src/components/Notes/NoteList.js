0dbe73179816730111d64b73de6bf8b6587e6070
leoplazavzla <Caracter$$123>
2023-01-19 11:53:52 -0300

    - Added masonry to the notes page with responsive capabilities

--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -6,14 +6,94 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import {CopyToClipboard} from "react-copy-to-clipboard";
+//import {Masonry} from "@mui/lab";
+import Masonry from 'react-masonry-css'
 
 
 const NoteList = () => {
     const {noteArray, deleteNotes, editNote, setNoteId, copyNoteContent} = useNote();
     const {currentUser} = useAuth();
+    const breakpoints = {
+        default: 3,
+        1100: 2,
+        700: 1
+    }
     return (
         <>
             {noteArray ?
+                <Masonry
+                    breakpointCols={breakpoints}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column"
+                >
+                    {noteArray.map((note, index) => {
+                        const title = <Typography color={"secondary"} variant={"h5"}>{note.name}</Typography>
+                        return(
+                            <div key={index}>
+                                <Card >
+                                    <CardHeader
+                                        color={"primary"}
+                                        title={title}
+                                        action={
+                                            <>
+                                                <IconButton
+                                                    color={"primary"}
+                                                    size="small"
+                                                    onClick={() => {
+                                                        editNote(note.id, note.name, note.description)
+                                                        setNoteId(note.id)
+                                                    }}
+                                                >
+                                                    <EditIcon/>
+                                                </IconButton>
+
+                                                <CopyToClipboard text= {note.description}>
+                                                    <IconButton
+                                                        color={"secondary"}
+                                                        size="small"
+                                                        onClick={() => {
+                                                            copyNoteContent(true)
+                                                        }}
+                                                    >
+                                                        <ContentCopyIcon/>
+                                                    </IconButton>
+                                                </CopyToClipboard>
+
+                                                <IconButton
+                                                    color={"error"}
+                                                    size="small"
+                                                    onClick={() => deleteNotes(note.id, currentUser.email)}
+                                                >
+                                                    <DeleteIcon/>
+                                                </IconButton>
+                                            </>
+                                        }
+                                    >
+                                    </CardHeader>
+
+                                    <CardContent>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                        >
+                                            {note.description}
+                                        </Typography>
+                                    </CardContent>
+
+                                </Card>
+                            </div>
+
+                        )
+
+                    })}
+
+                </Masonry>
+            :
+                <div>No notes yet. Please add your first note</div>
+            }
+
+
+            {/*{noteArray ?
                 <Grid container spacing={1}>
                     {noteArray.map((note) => {
                         const title = <Typography color={"secondary"} variant={"h5"}>{note.name}</Typography>
@@ -79,7 +159,7 @@ const NoteList = () => {
                 </Grid>
                 :
                 <div>No notes yet. Please add your first note</div>
-            }
+            }*/}
         </>
     )
 }
